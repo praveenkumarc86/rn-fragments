f718401e8662bc32a8966652c737dde26e8f4cd9
amandeepmittal <amandeepmittal@live.com>
2018-03-26 18:20:02 +0530

    4: class component

--- 01HelloWorld/App.js
+++ 01HelloWorld/App.js
@@ -1,5 +1,5 @@
 // import dependencies
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 // functional child component
@@ -7,14 +7,19 @@ const Hello = props => (
   <Text style={{ paddingTop: 50 }}>Hello, {props.name}</Text>
 );
 
-// functional parent component
-const AppComponent = () => {
-  return (
-    <View>
-      <Hello name="React Native" />
-      <Hello name="World" />
-    </View>
-  );
-};
+// class component
+// only use class component when
+// 1) to access lifecycle methods
+// 2) too have access to state
+class AppComponent extends Component {
+  render() {
+    return (
+      <View>
+        <Hello name="React Native" />
+        <Hello name="World" />
+      </View>
+    );
+  }
+}
 
 export default AppComponent;
