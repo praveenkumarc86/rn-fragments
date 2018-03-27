a1c77f4c8c235ba12bc63186f2b7623514ffa403
amandeepmittal <amandeepmittal@live.com>
2018-03-27 11:59:44 +0530

    add appTitle

--- 03stopwatch/App.js
+++ 03stopwatch/App.js
@@ -5,9 +5,8 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.appTitle}>StopWatch</Text>
+        <Text style={styles.appTitle}>StopWatch</Text>
       </View>
     );
   }
@@ -16,8 +15,12 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#eeeeee',
+    alignItems: 'center'
   },
+  appTitle: {
+    fontSize: 24,
+    fontWeight: '300',
+    paddingTop: 50
+  }
 });
