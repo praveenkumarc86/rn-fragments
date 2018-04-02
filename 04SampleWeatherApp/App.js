8515c9b0a9924f7cd0ae8c1262f6d68a1ccc8f96
amandeepmittal <amandeepmittal@live.com>
2018-04-02 18:21:47 +0530

    handle user input

--- 04SampleWeatherApp/App.js
+++ 04SampleWeatherApp/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zip: ''
+    };
+  }
+
+  // by using fat-arrow makes sure that the callback is bound to component instance (i.e. this)
+  _handleTextChange = event => {
+    this.setState({
+      zip: event.nativeEvent.text
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.welcome}>You Input {this.state.zip}</Text>
+        <TextInput
+          style={styles.input}
+          onSubmitEditing={this._handleTextChange}
+        />
       </View>
     );
   }
@@ -16,8 +32,22 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5fcff',
+    justifyContent: 'center'
   },
+  welcome: {
+    textAlign: 'center',
+    margin: 10,
+    fontSize: 20
+  },
+  input: {
+    fontSize: 20,
+    borderWidth: 2,
+    height: 40,
+    padding: 2,
+    height: 40,
+    width: 100,
+    textAlign: 'center'
+  }
 });
