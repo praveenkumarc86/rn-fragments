584291f6d261f2cb9dbbb24dec464e78f7129c1b
amandeepmittal <amandeepmittal@live.com>
2018-04-10 16:39:10 +0530

    default background

--- random-backgrounds/App.js
+++ random-backgrounds/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    defaultColor: '#bbb'
+  };
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View
+        style={[styles.container, { backgroundColor: this.state.defaultColor }]}
+      >
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Click Here</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -14,8 +21,16 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  button: {
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: '#00bbbb'
+  },
+  buttonText: {
+    fontSize: 18
+  }
 });
